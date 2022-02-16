29a58c96d9ca4ec605a8f7db5453976ffb7a8831
Dipak Patel <dipatel@mobiquityinc.com>
2022-02-16 17:08:41 +0530

    16 Feb commit

--- src/test/java/com/mob/casestudy/digitalbanking/service/CustomerSecurityImagesServiceTest.java
+++ src/test/java/com/mob/casestudy/digitalbanking/service/CustomerSecurityImagesServiceTest.java
@@ -6,6 +6,8 @@ import com.digitalbanking.openapi.model.GetCustomerSecurityImageResponse;
 import com.mob.casestudy.digitalbanking.entity.Customer;
 import com.mob.casestudy.digitalbanking.entity.CustomerSecurityImages;
 import com.mob.casestudy.digitalbanking.entity.SecurityImages;
+import com.mob.casestudy.digitalbanking.exception.CustomNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -43,5 +45,14 @@ class CustomerSecurityImagesServiceTest {
 
         ResponseEntity<GetCustomerSecurityImageResponse> actual = customerSecurityImagesService.retrieveSecurityImageByUserName(name);
         org.assertj.core.api.Assertions.assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
+
+    @Test
+    void getSecurityImageByUserName__withNullCustomerSecurityImage_shouldThrowException() {
+        String name = "Dipak";
+        Customer customer = new Customer();
+        customer.setCustomerSecurityImages(null);
+        Mockito.when(customerService.findCustomerByUserName(name, CUS_SEC_IMG_CUS_NOT_FOUND, "User " + name + " not found")).thenReturn(customer);
+        Assertions.assertThrows(CustomNotFoundException.class, () -> customerSecurityImagesService.retrieveSecurityImageByUserName(name));
     }
 }
